d8a66430315c0fdd7ab5e518cd0de28bf4d0f39f
reshmarsolanki <hello@reshmasolanki.com>
2025-11-07 14:55:21 -0600

    Adding all files from local machine to GitHub

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Add shadow to navigation on scroll
+//window.addEventListener('scroll', () => {
+   // const header = document.querySelector('.header');
+   // if (window.scrollY > 0) {
+      //  header.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.1)';
+   // } else {
+   //     header.style.boxShadow = 'none';
+   // }
+//});
